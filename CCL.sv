a34c3e0051b87bf5948be07e143106b1246f383a
Talin-Estiban <124513647+Talin-Estiban@users.noreply.github.com>
2023-08-28 21:32:54 +0300

    CCL design
    corrected task errors, but there are still errors in the internal stack

--- CCL.sv
+++ CCL.sv
@@ -1,19 +1,19 @@
 // CCL
-`define CCLUDepth 'd16; // this is the size of the stack
+`define CCLUDepth 'd16 // this is the size of the stack
 module CCLU	 ( input [1:0] command,
               input [31:0] address,
               input [31:0] counter, 
               input [31:0] inTarget,
               input bit reset,
               input clock,
-              output [31:0] outTarget,
-              output bit valid, 
-              output bit full, 
-              output bit error);
+              output reg [31:0] outTarget,
+              output reg valid, 
+              output reg full, 
+              output reg error)
   
   // define stack 
-  reg unsigned [31:0] CClAdress [CCLUDepth];
-  reg unsigned [31:0] count [CCLUDepth];
+  reg [31:0] CCladdress [`CCLUDepth];
+  reg [31:0] count [`CCLUDepth];
   
   // define stack index/pointer
   reg [3:0] index=0;
@@ -52,11 +52,11 @@ module CCLU	 ( input [1:0] command,
       	valid <= 1'b0;
       end
       else if ( command == 2'b10) begin
-        // examin top element and call task continue / task ccl
+        // examine  top element and call task continue / task ccl
         error <= 1'b0;
-        if ( adress == CCLAdress[index] && !full)
+        if ( address == CCLaddress[index] && !full)
           continueTask();
-        else if ( adress != CCLAdress[index])
+        else if ( address != CCLaddress[index])
           CCLTask();
       end
       else if (command == 2'b01) begin
@@ -68,8 +68,7 @@ module CCLU	 ( input [1:0] command,
   end
   
   // continue task
-  task continueTask () begin
-  
+  task automatic continueTask ();
     full <= 1'b0;
     valid <= 1'b1;
     outTarget <= inTarget;
@@ -77,42 +76,46 @@ module CCLU	 ( input [1:0] command,
       index<=index-1;
     else 
       count[index] <= count[index]-1;
-  end
-    
+
+  endtask
   
-    task CCLTask () begin
-      if (count[index] == 1'b0) begin
+  //CCL task
+  task automatic CCLTask ();
+    if (count[index] == 1'b0) begin
         error = 1'b1;
         valid<= 1'b0;
         full<=1'b0;
       end
-      else if (count[index] == 1'b1) begin
+    else if (count[index] == 1'b1) begin
         index<=index-1;
         error<=1'b0;
         valid<=1'b1;
         outTarget<=inTarget;
         full<=1'b0;
-      end
-      else if ( index==16) begin
+    end
+    else if ( index==16) begin
         full<=1'b1;
         error<=1'b1;
         valid<=1'b0;
-      end
-      else begin
+    end
+    else begin
         count[index+1]<=counter-1;
-        CCLAdress[index+1] <= address;
+      	CCLaddress[index+'d1] <= address;
         outTarget<=inTarget;
         valid<=1'b1;
         full<=1'b0;
-      end
     end
-      task breakTask () begin
-        full<=1'b;
-        valid<=1'b;
-       	outTarget<=inTarget;
-        error<=1'b0;
-        index=index-1;
-      end
+  endtask
+  
+  // break task
+  task automatic breakTask();
+     full<=1'b0;
+     valid<=1'b0;
+     outTarget<=inTarget;
+     error<=1'b0;
+     index=index-1;
+  endtask
+
 endmodule
 
       
