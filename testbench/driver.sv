a99e3f274b4114a253bd7c531b83e92b90333deb
Talin Estiban <124513647+Talin-Estiban@users.noreply.github.com>
2023-10-19 11:38:59 +0300

    final driver code

--- testbench/driver.sv
+++ testbench/driver.sv
@@ -12,5 +12,22 @@ class cclu_driver extends uvm_driver#(cclu_sequence_item);
     super.build_phase(phase)
   endfunction: build
   //run phase
+  virtual task run_pahse (uvm_phase pahse);
+    forever begin 
+      seq_item_port.get_next_item(req);
+      drive();
+      seq_item_port.item_done();
+    end
+  endtask: run_phase
   // driver 
+  virtual task drive();
+    req.print();
+    @(posedge vif.clock) begin
+      vif.command <= req.command;
+      vif.address <= req.address;
+      vif.counter <= req.counter;
+      vif.inTarget <= req.inTarget;
+      vif.reset <= req.reset;
+    end
+  endtask: drive
 endclass: cclu_driver
